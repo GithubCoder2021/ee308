e1429308f723bda4087d9ce89291ff116a6fa088
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-22 15:48:12 -0500

    Update ee308.js

--- ee308.js
+++ ee308.js
@@ -2,16 +2,43 @@
 function normal(log) {
   if (log != Infinity) {
     if (log >= 1e21) {
-      return "e" + log;
+       return "e" + log;
     } else if (log >= 6) {
-      return (Math.round(10**(log - Math.floor(log))*1000))/1000 + "e" + Math.floor(log);
+       return (Math.round(10**(log - Math.floor(log))*1000))/1000 + "e" + Math.floor(log);
     } else if (log >= -Infinity) {
-      return Math.floor(10 ** log);
+       return Math.floor(10 ** log);
     } else {
-      return 0;
+       return 0;
     }
   } else {
    return "Eternity";
   }
-  
+}
+
+function addLogs(a,b){
+  if (a > b) {
+    return a + (Math.log10(1+(10**(b - a))));
+  } else {
+    return a + (Math.log10(1+(10**(a - b))));
+  }
+}
+
+function subtractLogs(a,b){
+  if (a > b) {
+    return a + (Math.log10(1+(0 -(10**(b - a)))));
+  } else {
+    return -Infinity;
+  }
+}
+
+function multiplyLogs(a,b) {
+  return a + b;
+}
+
+function divideLogs(a,b) {
+  return a - b;
+}
+
+function exponentLoga(a,b){
+  return a * b;
 }
