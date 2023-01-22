42196fa0be3c719d21816fe794e063636dda54e8
GithubCoder2021 <91566024+GithubCoder2021@users.noreply.github.com>
2023-01-22 12:45:38 -0500

    Update ee308.js

--- ee308.js
+++ ee308.js
@@ -1,4 +1,17 @@
 
 function normal(log) {
+  if (log != Infinity) {
+    if (log >= 1e21) {
+      return "e" + log;
+    } else if (log >= 6) {
+      return (Math.round(10**(log - Math.floor(log))*1000))/1000 + "e" + Math.floor(log);
+    } else if (log >= -Infinity) {
+      return Math.floor(10 ** log);
+    } else {
+      return 0;
+    }
+  } else {
+   return "Eternity";
+  }
   
 }
